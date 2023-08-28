828421fd86cb79973b521490dc17c7cda7d496fe
Ashwin049 <achuashwin049@gmail.com>
2023-08-28 08:34:08 +0530

    fir

--- bin/dart_application_2.dart
+++ bin/dart_application_2.dart
@@ -1,16 +1,11 @@
-
 import 'dart_application_2.dart';
 
 import 'clas.dart';
 
-import 'class1.dart'; 
+import 'class1.dart';
 
- 
-void main()
-{
-  var shilpa=new ashwin();//object
+void main() {
+  var shilpa = new ashwin(); //object
   shilpa.GetName();
   shilpa.NumIn();
-
 }
-
